34b601d1cca109435d1d75eba85784b92d3a7db8
Markus Klemm <markus@markus@markusklemm.net>
2017-06-05 00:17:51 +0200

    Lesson name reading

--- php/vokabel.php
+++ php/vokabel.php
@@ -3,12 +3,40 @@ $http_method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
 
-if (file_put_contents("/home/rex/fi3/ia13/s70357/public_html/woot.txt", $http_method . "\n===\n" . $request . "\n=====\n" . $input) == FALSE)
+class lesson
 {
-    header('HTTP/1.1 500');
+    public $name = '';
+    public $language1 = array('');
+    public $language2 = array('');
+    public $pronunciation = array('');
+
 }
-else
-{
-    header('HTTP/1.1 200');
+
+function get_lessons() {
+    $lessons = array();
+    $directory = __DIR__ . '/lessons';
+    $iterator = new DirectoryIterator ( $directory );
+    foreach ( $iterator as $fileInfo ) {
+        if (!$fileInfo->getExtension() == "txt") {
+            continue;
+        }
+        $lesson = new lesson();
+        $lesson->name = preg_replace('#.txt$#', '', $iterator->getFilename());
+        $lessons[] = $lesson;
+
+    }
+    return json_encode($lessons);
 }
+
+
+$json = '{}';
+switch ($http_method) {
+    case 'GET':
+        $json = get_lessons();
+        break;
+}
+
+header('HTTP/1.1 200');
+header("Content-Type: application/json; charset=UTF-8");
+echo $json;
 ?>
--- public/es6/vokabeltrainer.js
+++ public/es6/vokabeltrainer.js
@@ -9,11 +9,19 @@ $(function(){
     $('#stats_menu').click(function () {
         $('#stats_page').removeClass('hidden');
         $('#lesson_choice_page').addClass('hidden');
+        $('#setup_page').addClass('hidden');
     })
     $('#lesson_menu').click(function () {
         $('#lesson_choice_page').removeClass('hidden');
         $('#stats_page').addClass('hidden');
+        $('#setup_page').addClass('hidden');
     })
+    $('#setup_menu').click(function () {
+        $('#setup_page').removeClass('hidden');
+        $('#stats_page').addClass('hidden');
+        $('#lesson_choice_page').addClass('hidden');
+    })
+
 
 
     console.log("I Ran");
