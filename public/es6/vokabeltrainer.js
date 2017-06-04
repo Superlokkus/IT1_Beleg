8bdf882e100ef76e85a532b832058e5eae5acb78
Markus Klemm <markus@markus@markusklemm.net>
2017-06-04 22:49:40 +0200

    Now this damn navigation works at least basic

--- public/es6/vokabeltrainer.js
+++ public/es6/vokabeltrainer.js
@@ -6,5 +6,16 @@ $(function(){
        console.log("Clicked");
        $('#btn_3').addClass('disabled');
     });
+    $('#stats_menu').click(function () {
+        $('#stats_page').removeClass('hidden');
+        $('#lesson_choice_page').addClass('hidden');
+    })
+    $('#lesson_menu').click(function () {
+        $('#lesson_choice_page').removeClass('hidden');
+        $('#stats_page').addClass('hidden');
+    })
+
+
     console.log("I Ran");
 });
+
