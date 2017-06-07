eb327be74dee8dd10ecbb895e69b93d0d307de4f
Markus Klemm <markus@markus@markusklemm.net>
2017-06-07 19:08:17 +0200

    Linting added

--- public/es6/vokabeltrainer.js
+++ public/es6/vokabeltrainer.js
@@ -1,29 +1,24 @@
-'use strict';
+"use strict";
 
 $(function(){
-    $('#btn_2').addClass('disabled');
-    $('#btn_1').click(function(){
-       console.log("Clicked");
-       $('#btn_3').addClass('disabled');
+    $("#stats_menu").click(function () {
+        $("#stats_page").removeClass("hidden");
+        $("#lesson_choice_page").addClass("hidden");
+        $("#setup_page").addClass("hidden");
     });
-    $('#stats_menu').click(function () {
-        $('#stats_page').removeClass('hidden');
-        $('#lesson_choice_page').addClass('hidden');
-        $('#setup_page').addClass('hidden');
-    })
-    $('#lesson_menu').click(function () {
-        $('#lesson_choice_page').removeClass('hidden');
-        $('#stats_page').addClass('hidden');
-        $('#setup_page').addClass('hidden');
-    })
-    $('#setup_menu').click(function () {
-        $('#setup_page').removeClass('hidden');
-        $('#stats_page').addClass('hidden');
-        $('#lesson_choice_page').addClass('hidden');
-    })
+    $("#lesson_menu").click(function () {
+        $("#lesson_choice_page").removeClass("hidden");
+        $("#stats_page").addClass("hidden");
+        $("#setup_page").addClass("hidden");
+    });
+    $("#setup_menu").click(function () {
+        $("#setup_page").removeClass("hidden");
+        $("#stats_page").addClass("hidden");
+        $("#lesson_choice_page").addClass("hidden");
+    });
 
+    let lesson_btn = $('<button type="button" id="btn_1" class="btn">Apple</button>');
+    $("#lesson_list_div").append(lesson_btn);
 
-
-    console.log("I Ran");
 });
 
